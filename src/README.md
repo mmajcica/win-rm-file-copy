b7cd50336ffc2a8a8fe563032317a8bcac4a2511
Majcica, M (Mario) <Mario.Majcica@rabobank.com>
2017-12-01 10:53:35 +0100

    Initial commit

---
+++ src/README.md
@@ -0,0 +1,32 @@
+# WinRmFileCopy `0.1.0`
+Windows Machine File Copy (WinRM)
+
+> INSTRUCTIONS
+>
+> Your shiny new extension is ready! Try `gulp package` to check out :)
+>
+> Customize your extension as you like...
+> * Replace the logo.png, logo_large.svg files with your extension\'s logo.
+> * Replace the icon.png, icon.svg files with your task\'s logo. We recommend inkscape to create these files.
+> * Update ' + this.destinationPath('README.md'));
+>
+> Or publish the vsix if you\'re feeling adventurous.
+>
+> If you have feedback/bugs/praise for us, share it at
+> http://github.com/codito/generator-vsts-task/issues
+> We welcome patches/pull requests too ;)
+
+> EDIT ME
+> This content will show up in the Visualstudio Marketplace
+> homepage for your extension
+
+# Configuration
+
+> TODO
+> Show how to configure the extension
+
+# Usage
+
+> TODO
+> And then document the primary flows. Feel free to link to
+> your repository or website for detailed steps.
---
+++ src/win-rm-file-copy/WinRmFileCopy.ps1
@@ -0,0 +1,153 @@
+[CmdletBinding()]
+param()
+
+Trace-VstsEnteringInvocation $MyInvocation
+
+try
+{
+    # Get inputs for the task
+    [string]$machineNames = Get-VstsInput -Name MachineNames -Require
+    [string]$adminUserName = Get-VstsInput -Name AdminUserName -Require
+    [string]$adminPassword = Get-VstsInput -Name AdminPassword -Require
+    [string]$sourcePath = Get-VstsInput -Name SourcePath -Require
+    [string]$targetPath = Get-VstsInput -Name TargetPath -Require
+    [bool]$cleanTargetBeforeCopy = Get-VstsInput -Name CleanTargetBeforeCopy -AsBool
+    [bool]$copyFilesInParallel = Get-VstsInput -Name CopyFilesInParallel -AsBool
+
+    $sourcePath = $sourcePath.Trim('"')
+    $targetPath = $targetPath.Trim('"')
+
+    # Normalize admin username
+    if($adminUserName -and (-not $adminUserName.StartsWith(".\")) -and ($adminUserName.IndexOf("\") -eq -1) -and ($adminUserName.IndexOf("@") -eq -1))
+    {
+        $adminUserName = ".\" + $adminUserName 
+    }
+
+    if(-not (Test-Path -LiteralPath $sourcePath))
+    {
+        throw "Source path '$sourcePath' does not exist."
+    }
+    else
+    {
+        $path = Get-Item $sourcePath
+        
+        if ($path -is [System.IO.DirectoryInfo] -and (-not ($sourcePath.EndsWith("\"))))
+        {
+            $sourcePath += "\*"
+        }
+    }
+
+    if (-not ([System.IO.Path]::IsPathRooted($targetPath)))
+    {
+        throw "Target path needs to contain root folder as 'C:\'"
+    }
+
+    if (-not ($targetPath.EndsWith("\")))
+    {
+        $targetPath += "\"
+    }
+
+    Write-Output "Files from '$sourcePath' will be copied to the path '$targetPath' on the remote machine(s)."
+
+    $machines = $machineNames.split(',') | ForEach-Object { if ($_ -and $_.trim()) { $_.trim() } }
+
+    $secureAdminPassword = ConvertTo-SecureString $adminPassword -AsPlainText -Force
+    $machineCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminUserName, $secureAdminPassword
+
+    if ($machines.Count -eq 0)
+    {
+        throw "No machine exists under environment: '$machineNames' for deployment"
+    }
+
+    $copyJob = {
+        param
+        (
+            [string]$fqdn, 
+            [string]$sourcePath,
+            [string]$targetPath,
+            [System.Management.Automation.PSCredential]$credential,
+            [bool]$cleanTargetBeforeCopy
+        )
+
+        try
+        {
+            $session = New-PSSession -ComputerName $fqdn -Credential $credential
+
+            $testPath = {
+                param ([string]$targetPath)
+        
+                if (-not (Test-Path -LiteralPath $targetPath -IsValid -PathType Container))
+                {
+                    throw "Destination path is not a valid path."
+                }
+        
+                if (-not (Test-Path -LiteralPath $targetPath -PathType Container))
+                {
+                    New-Item -ItemType Directory -Force -Path $targetPath | Out-Null
+                }
+            }
+
+            Invoke-Command -Session $session -ScriptBlock $testPath -ArgumentList $targetPath
+
+            if ($cleanTargetBeforeCopy)
+            {
+                Invoke-Command -Session $session -ScriptBlock { param ([string]$path) Get-ChildItem $path -Force | Remove-Item -Recurse -Force } -ArgumentList $targetPath
+            }
+
+            Copy-Item -Path $sourcePath -Destination $targetPath -Force -Recurse -Container -ToSession $session
+        }
+        finally
+        {
+            Write-Verbose "Closing Powershell remote session on $fqdn.";
+            if ($session -ne $null) { $session | Disconnect-PSSession | Remove-PSSession }
+        }
+    }
+
+    if($copyFilesInParallel -eq $false -or  ( $machines.Count -eq 1 ))
+    {
+        foreach($machine in $machines)
+        {
+            Write-Output "Copy started for -  $machine"
+
+            Invoke-Command -ScriptBlock $CopyJob -ArgumentList $machine, $sourcePath, $targetPath, $machineCredential, $cleanTargetBeforeCopy
+        } 
+    }
+    else
+    {
+        [hashtable]$Jobs = @{} 
+
+        foreach($machine in $machines)
+        {
+            Write-Output "Copy started for -  $machine"
+
+            $job = Start-Job -ScriptBlock $CopyJob -ArgumentList $machine, $sourcePath, $targetPath, $machineCredential, $cleanTargetBeforeCopy
+
+            $Jobs.Add($job.Id, $machine)
+        }        
+
+        While ($Jobs.Count -gt 0)
+        {
+            Start-Sleep 3 
+            foreach($job in Get-Job)
+            {
+                if($Jobs.ContainsKey($job.Id) -and $job.State -ne "Running")
+                {
+                    Receive-Job -Id $job.Id
+                    Remove-Job $Job                 
+                    $Jobs.Remove($job.Id)
+                } 
+            }
+        }
+    }
+
+    Write-Output "Copy succeeded."
+}
+catch
+{
+    Write-Verbose $_.Exception.ToString() -Verbose
+    throw
+}
+finally
+{
+    Trace-VstsLeavingInvocation $MyInvocation
+}
